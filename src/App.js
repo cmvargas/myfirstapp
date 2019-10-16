c4711f1ae5e4d8aebffa2826c8314cbff9b4aaeb
cristian.vargas <cristian.vargas@tars.dev>
2019-10-16 17:29:29 -0500

    Learn components and state in react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// function Helloworld(props){
+// return (
+//   <div id="hello">
+//     <h1>{props.subtitle}</h1>
+//     {props.mytext}
+//   </div>
+// );
+// }
 
+class Helloworld extends React.Component{
+  state = {
+    show: true
+  };
+  toggleShow=()=>{
+    this.setState({show: !this.state.show});
+  }
+  render(){
+    if (this.state.show){
+      return (
+        <div id="hello">
+          <h1>{this.props.subtitle}</h1>
+          {this.props.mytext}
+          <button onClick={this.toggleShow}>Toggle Show</button>
+        </div>
+      );
+    }
+    else{
+      return (
+      <div>
+        There are not elements
+        <button onClick={this.toggleShow}>Toggle show</button>
+      </div>);
+    }
+    
+  }
+}
+
+// const App = () => <div><Helloworld/></div>
+
+// class App extends React.Component{
+//   render(){
+//     return (<div>This is my component: <Helloworld/></div>);
+//   }
+// }
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is my component: 
+      <Helloworld mytext="Hello fazt" subtitle="loremp ispum"/> 
+      <Helloworld mytext="Hola mundo" subtitle="component two"/> 
+      <Helloworld mytext="Hello!" subtitle="component three"/> 
     </div>
   );
 }
